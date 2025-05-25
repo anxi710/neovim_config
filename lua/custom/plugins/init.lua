56b1fcccc9b3719a666ea75ee399d234190b718a
anxi <anxi710@outlook.com>
2025-05-25 16:28:06 +0800

    vim-tpipeline config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -180,4 +180,13 @@ return {
       }
     end,
   },
+  {
+    'vimpostor/vim-tpipeline',
+    config = function()
+      -- 自动绑定 vim 状态栏到 tmux
+      vim.g.tpipeline_autoembed = 1
+      vim.g.tpiptline_restore = 1
+      vim.g.tpipeline_clearstl = 1
+    end,
+  },
 }
