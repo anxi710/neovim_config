1ffdb2d7e46e7b572cc675f32a03dc2d832b6d76
anxi <anxi710@outlook.com>
2025-05-25 16:25:16 +0800

    toggleterm.nvim configuration

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,23 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    config = function()
+      require('toggleterm').setup()
+
+      local Terminal = require('toggleterm.terminal').Terminal
+      local lazygit = Terminal:new {
+        cmd = 'lazygit',
+        hidden = true,
+        direction = 'float',
+      }
+
+      vim.keymap.set('n', '<Leader>lg', function()
+        lazygit:toggle()
+      end, { desc = 'Lazygit' })
+    end,
+  },
+}
