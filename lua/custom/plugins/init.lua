5e0ee7db6b943d3ac1a0c2c4a4b4adf8ea699e4e
anxi <anxi710@outlook.com>
2025-05-25 16:27:20 +0800

    lualine.nvim config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -158,4 +158,26 @@ return {
       })
     end,
   },
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('lualine').setup {
+        options = {
+          -- theme = 'Catppuccin',
+          section_separators = '',
+          component_separators = '',
+          icons_enabled = true,
+        },
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch', 'diff', 'diagnostics' },
+          lualine_c = { 'filename' },
+          lualine_x = { 'encoding', 'fileformat', 'filetype' },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' },
+        },
+      }
+    end,
+  },
 }
