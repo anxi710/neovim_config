3c875b0f78a0705788739abbf4b7e97b1dd4b160
anxi <anxi710@outlook.com>
2025-05-27 23:31:48 +0800

    不使用 catppuccin 插件来设置颜色
    由于我的 kitty 使用的是 catppuccin 配色，所以这里删除该插件，保留终端模拟器的配色方案

--- lua/custom/plugins/catppuccin.lua
+++
@@ -1,9 +0,0 @@
--- lua/custom/plugins/catppuccin.lua
-return {
-  'catppuccin/nvim',
-  name = 'catppuccin',
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme 'catppuccin-mocha'
-  end,
-}
