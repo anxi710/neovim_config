11c89a44ff9b9604b8045caeb5d73ce3e6879f57
anxi <anxi710@outlook.com>
2025-05-30 00:24:15 +0800

    配置 folding, encoding, tab；添加空白字符显示

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -7,10 +7,6 @@ vim.g.python3_host_prog = '/usr/bin/python3'
 
 vim.g.perl_host_prog = '/usr/bin/perl'
 
--- vim.opt.tabstop = 4
--- vim.opt.shiftwidth = 4
--- vim.opt.expandtab = true
-
 -- 自动切换相对行号
 vim.opt.relativenumber = true -- 设置初始状态为相对行号
 vim.api.nvim_create_autocmd({ 'InsertEnter' }, {
@@ -40,3 +36,35 @@ vim.g.lazygit_config_file_path = '' -- custom config file path
 vim.g.lazygit_config_file_path = {} -- table of custom config file paths
 
 vim.g.lazygit_on_exit_callback = nil -- optional function callback when exiting lazygit (useful for example to refresh some UI elements after lazy git has made some changes)
+
+-- Folding
+-- foldminlines & foldnestmax
+vim.wo.foldenable = true
+-- 使用 nvim-ufo 来智能化处理 fold，因此不需要下面两行
+-- vim.wo.foldmethod = 'expr'
+-- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+
+-- Tab
+vim.o.tabstop = 4 -- tab 显示为 4 个空格宽
+vim.o.shiftwidth = 4 -- 每次缩进使用 4 个空格
+vim.o.softtabstop = 4 -- Tab 键时退格/缩进行为
+vim.o.expandtab = true -- 将 Tab 转为空格
+
+vim.o.fileencoding = 'utf-8'
+vim.o.encoding = 'utf-8'
+
+vim.o.autoindent = true -- 启用自动缩进
+vim.o.smartindent = true -- 智能缩进（适用于 C 风格语言）
+vim.o.smarttab = true -- Tab 键行为更智能
+vim.o.breakindent = true -- 换行时保留缩进
+
+-- 开启空白字符显示
+vim.o.list = true
+vim.opt.listchars = {
+  -- space = '·', -- 空格
+  tab = '» ', -- 制表符（Tab），后面有空格表示对齐
+  trail = '·', -- 行尾多余空格
+  extends = '›', -- 超出右侧边界
+  precedes = '‹', -- 超出左侧边界
+  nbsp = '␣', -- 不换行空格（non-breaking space）
+}
