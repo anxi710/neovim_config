734fe691d0473003c357cc038ef66b33a2decb4b
anxi <anxi710@outlook.com>
2025-05-30 12:41:24 +0800

    telescope 添加 aerial 集成

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -65,11 +65,30 @@ return {
         ['ui-select'] = {
           require('telescope.themes').get_dropdown(),
         },
+        ['aerial'] = {
+          -- Set the width of the first two columns (the second
+          -- is relevant only when show_columns is set to 'both')
+          col1_width = 4,
+          col2_width = 30,
+          -- How to format the symbols
+          format_symbol = function(symbol_path, filetype)
+            if filetype == 'json' or filetype == 'yaml' then
+              return table.concat(symbol_path, '.')
+            else
+              return symbol_path[#symbol_path]
+            end
+          end,
+          -- Available modes: symbols, lines, both
+          show_columns = 'both',
+
+          vim.keymap.set('n', '<leader>so', '<cmd>Telescope aerial<CR>', { desc = 'Telescope Symbol Outline' }),
+        },
       },
     }
 
     -- Enable Telescope extensions if they are installed
     pcall(require('telescope').load_extension, 'fzf')
+    pcall(require('telescope').load_extension, 'aerial')
     pcall(require('telescope').load_extension, 'ui-select')
 
     -- See `:help telescope.builtin`
