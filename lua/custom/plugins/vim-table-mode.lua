0b442877a9bcde11b6e2b0abddca63c7491b0673
anxi <anxi710@outlook.com>
2025-06-06 16:46:05 +0800

    增强 markdown 文档书写体验

---
+++ lua/custom/plugins/vim-table-mode.lua
@@ -0,0 +1,8 @@
+return {
+  'dhruvasagar/vim-table-mode',
+  cmd = { 'TableModeToggle' },
+  ft = { 'markdown' },
+  keys = {
+    { '<leader>tm', '<cmd>TableModeToggle<cr>', desc = 'Toggle Table Mode' },
+  },
+}
